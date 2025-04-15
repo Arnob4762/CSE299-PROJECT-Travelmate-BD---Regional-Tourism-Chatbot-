28043d2556ff0185a89d7af5a659c62872d2d71c
AZMAIN ARNOB <ornobazmain@gmail.com>
2025-04-15 19:33:52 +0600

    Update performance_analyzer.py

--- performance_analyzer.py
+++ performance_analyzer.py
@@ -1,6 +1,4 @@
-from core_utils import get_performance_report
+from core_utils import get_performance_report as core_get_performance_report
 
-# The analyze_performance function is no longer used to run a new query.
-# Instead, we simply expose the performance report.
 def get_performance_report():
-    return get_performance_report()
+    return core_get_performance_report()
